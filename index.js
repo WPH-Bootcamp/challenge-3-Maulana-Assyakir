033d507467c6fe69cca7838c7167a08031154bbe
Maulana-Assyakir <maulanaassyakir28@gmail.com>
2025-11-16 18:06:21 +0700

    feat: challenge done

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// code here, goodluck!!
+'use strict';
+// 1. User Input Handling
+let prompt = require("prompt-sync")();
+
+function getValidNumberInput(prompMessage) {
+    let num;
+    let flag = 0;
+    do {
+        flag++;
+        if (flag > 1)
+            console.log("input tidak valid, silahkan masukkan angka yang benar.");
+        num = prompt(prompMessage);
+    } while (isNaN(num) || num.trim() === ''); 
+    return parseFloat(num);}
+{}
+
+// funciton
+function calculation(a, b, operator) {
+    switch (operator) {
+        case "+": return a + b;
+        case "-": return a-b;
+        case "*": return a*b;
+        case "/": return a/b;
+        default: return "operator tidak valid";
+    }
+}
+
+console.log("Calculator Simple");
+
+while (true) {
+    let angka = getValidNumberInput("Masukkan angka pertama: ");
+
+    console.log("typeof: ", typeof angka); // typeof angka, berarti tipe datanya angka
+
+    let angka2 = getValidNumberInput("Masukan angka kedua: ");
+
+    let operator = prompt ("Masukkan operator (+, -, *, /): ");
+
+    let hasil = calculation(parseInt(angka), parseInt(angka2), operator);
+
+    console. log("Hasilnya adalah : ", hasil);
+
+    let question = prompt ("Apakah Anda ingin lanjut? (y/n): ");
+
+    if (question.toLowerCase() === "n") {
+        console. log("Terima kasih.");
+        break;
+    }
+}
+
+    console.log("Kalkulator selesal.");
